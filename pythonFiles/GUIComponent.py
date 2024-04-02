684adea6b55b53c3b5e5427fbc513df7df69e5b3
Angel Mosquera <angelm@wolfram.com>
2024-04-02 00:19:44 -0500

    time panel added, trial panel in progress

--- pythonFiles/GUIComponent.py
+++ pythonFiles/GUIComponent.py
@@ -1,5 +1,6 @@
 from tkinter import ttk
 import tkinter as tk
+import time
 
 class AppPanel:
 
@@ -11,9 +12,10 @@ class AppPanel:
         self.frame.pack(expand=True,fill="both")
 
         ##Serial conection panel
-        zone1 = tk.Frame(self.frame, bg="lightblue",width=180,height=270)
-        zone1.pack(expand=True, anchor="nw", fill="none")
+        zone1 = tk.Frame(self.frame, bg="lightblue",width=180,height=180)
+        zone1.pack(expand=False, anchor="nw", fill="none", padx=5, pady = 5)
 
+        self.serialConnect = False
         panelName = tk.Label(zone1, text="Serial Connection Panel")
         panelName.pack(anchor="nw",padx = 10, pady=5, ipady=5)
         comSelect = ttk.Combobox(zone1,values =[1,2,3,4],width=60)
@@ -21,20 +23,63 @@ class AppPanel:
         baudSelect = tk.Entry(zone1,width=63)
         baudSelect.pack(side="top",anchor="w",pady=2, padx=10, ipadx=5)
 
-        zone2 = tk.Frame(zone1,bg = "lightblue",width=100,height=100)
-        zone2.pack(side="left", padx=10, pady=5)
-        self.connectButton = tk.Button(zone2,text="Connect",fg="black",command=self.connect)
+        self.connectTimeStart = 0
+        zone11 = tk.Frame(zone1,bg = "lightblue",width=100,height=100)
+        zone11.pack(side="left", padx=10, pady=5)
+        self.connectButton = tk.Button(zone11,text="Connect",fg="black",command=self.connect)
         self.connectButton.pack(side="left",padx=5,ipadx=10)
-        self.disconnectButton = tk.Button(zone2,text="Disconnect",fg="black",command=self.disconnect, state=tk.DISABLED)
+        self.disconnectButton = tk.Button(zone11,text="Disconnect",fg="black",command=self.disconnect, state=tk.DISABLED)
         self.disconnectButton.pack(side="left",padx =5,ipadx=10)
+
+        ##Clock panel
+        zone2 = tk.Frame(self.frame, bg="lightblue",width=180,height=250)
+        zone2.pack(expand=False, anchor="nw", fill="none", padx=5, pady = 5)
+
+        clockName = tk.Label(zone2, text="Clock Panel")
+        clockName.pack(anchor="nw",padx = 10, pady=5, ipady=5)
+        self.globalTime = tk.Label(zone2, text="",font=("Helvetica",48))
+        self.globalTime.pack(side= "top",anchor="w",padx=10,pady=5, ipadx=5)
+        self.connectionTime = tk.Label(zone2, text="00:00:00",font=("Helvetica",48))
+        self.connectionTime.pack(side= "top",anchor="w",padx=10,pady=5, ipadx=5)
+        self.timeRefresh()
+
+        ##Start-Stop-Save panel
+        zone3 = tk.Frame(self.frame, bg="lightblue",width=180,height=250)
+        zone3.pack(expand=False, anchor="nw", fill="none", padx=5, pady = 5)
         
+
     def connect(self):
         self.connectButton.config(state=tk.DISABLED)
         self.disconnectButton.config(state=tk.NORMAL)
+        self.serialConnect = True
+
+        self.connectTimeStart = time.time()
+        self.timeRefresh()
     
     def disconnect(self):
         self.connectButton.config(state=tk.NORMAL)
         self.disconnectButton.config(state=tk.DISABLED)
+        self.serialConnect = False
+        self.timeRefresh()
+
+    def format_seconds(self, seconds):
+        hours, remainder = divmod(seconds, 3600)
+        minutes, seconds = divmod(remainder, 60)
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+    def timeRefresh(self):
+        currentTime = time.strftime('%H:%M:%S')
+        self.globalTime.config(text = currentTime)
+
+        if self.serialConnect:
+            connectionTime = int(time.time() - self.connectTimeStart)
+            formattedTime = self.format_seconds(connectionTime)
+            self.connectionTime.config(text = formattedTime)
+        else:
+            self.connectionTime.config(text = "00:00:00")
+        
+        self.master.after(1000, self.timeRefresh)
+
         
 
 
